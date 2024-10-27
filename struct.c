f7dfcd81d2912967e869a7aacea0b065f2469a31
olamide <olamxii@gmail.com>
2024-10-27 19:27:17 +0200

    Structure

--- struct.c
+++ struct.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+struct Student {
+  char name[50];
+  char id[10];
+  int age;
+  int grade[5];
+};
 int main() {
   printf("Struct\n");
+  struct Student kelvin;
+  kelvin.age = 50;
+
+  printf("Age: %d\n", kelvin.age);
   return 0;
 }
